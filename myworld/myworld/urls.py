979c09751b2c5fa98248bc8df74de67d13ab037d
Tung <nttp1g10@ecs.soton.ac.uk>
2014-05-23 22:17:08 +0100

    Build the first view

--- myworld/myworld/urls.py
+++ myworld/myworld/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'myworld.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^worldapp/', include('worldapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- myworld/worldapp/views.py
+++ myworld/worldapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+	title = 'This is Tung\'s world'
+	context = {
+		'page_title': title,
+	}
+	return render(request, 'worldapp/index.html', context)
 # Create your views here.
